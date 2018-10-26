dfb5539d29fea028996004780295167173aed86c
oxygen545 <damon@hopkins-family.org>
2018-10-26 13:45:12 -0500

    cleaning up

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@ AdminUser.create!([
   {email: "admin@example.com", password: "password", password_confirmation: "password" },
 ])
 Home.create!([
-  {link: "Link" title: "Title", subtitle: "Subtitle", heading: "Heading", weight: 100, body:"<p>Body</p>" }
+  {link: "Link", title: "Title", subtitle: "Subtitle", heading: "Heading", weight: 100, body:"<p>Body</p>" }
 ])
