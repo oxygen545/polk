ddcba16a050a9720dd0de2a333956f11804e7be1
oxygen545 <damon@hopkins-family.org>
2018-11-02 03:49:33 -0500

    More config changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
 AdminUser.create!([
-  {email: "admin@example.com", password: "password", password_confirmation: "password" },
+  {email: "admin@example.com", password: "password", password_confirmation: "password"}
+])
+User.create!([
+  {username: "Damon Hopkins" email: "damon@hopkins-family.org", password: "password", password_confirmation: "password"}
 ])
