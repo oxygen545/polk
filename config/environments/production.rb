826a00e8b43480ab9d43c4ac8f3c80e126e5574b
oxygen545 <damon@hopkins-family.org>
2018-11-02 14:31:13 -0500

    FF to here already

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   # ActionMailer Config
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => 'adhoc.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'damonhopkins.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address              => 'smtp.gmail.com',
