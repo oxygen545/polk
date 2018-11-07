8c9be730da43e7b82bbfc095896454b9c54535d4
oxygen545 <damon@hopkins-family.org>
2018-11-06 19:19:54 -0600

    added admin and user login check to the users index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,13 @@ Rails.application.routes.draw do
   resources :photos
   root to: "visitors#cover"
   devise_for :users
+  resources :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get "/cover" => "visitors#cover"
   get "/features" => "visitors#features"
   get "/static_pages" => "visitors#static_pages"
   get "/dynamic_pages" => "visitors#dynamic_pages"
-  get "/pricing" => "visitors#pricing"
-  get "/jumbotron" => "visitors#jumbotron"
   get "/tech_stack" => "visitors#tech_stack"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
