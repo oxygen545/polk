2fec85726178fb2a5563ac9a29b21bc004050b61
oxygen545 <damon@hopkins-family.org>
2018-11-13 16:49:04 -0600

    adding stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-User.create!([
-  {username: "Damon Hopkins", email: "damon@hopkins-family.org", encrypted_password: "$2a$11$8CDG9793r6aLHDbRe1O8vuB5wjhGIBdfIQz.X8WjYuI/TaHvohjwa", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2018-11-12 03:48:06", last_sign_in_at: "2018-11-12 03:48:06", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", confirmation_token: "qVJ1Hnb1WHqUFzne-BCh", confirmed_at: nil, confirmation_sent_at: "2018-11-12 03:45:45", unconfirmed_email: nil}
-])
 AdminUser.create!([
-  {email: "admin@example.com", encrypted_password: "$2a$11$dvNucDtbYsox0SSJu7JEv.sbfe2Kn5qTlKHSZye7n86V6gyhDwDNO", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil}
+  {email: "admin@example.com", password: "password", password_confirmation: "password"}
+])  
+User.create!([
+  {username: "Damon Hopkins", email: "damon@hopkins-family.org", password: "password", password_confirmation: "password"}
 ])
 Photo.create!([
   {page_name: "cover", caption: "Jenny and baby Lizzy 1996", sequence: 1},
