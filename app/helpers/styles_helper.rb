d2e285d548a83369a54fa0bcad897693092a437e
oxygen545 <damon@hopkins-family.org>
2018-11-14 08:44:29 -0600

    fixing the bugs in the features page

--- app/helpers/styles_helper.rb
+++ app/helpers/styles_helper.rb
@@ -27,7 +27,7 @@ module StylesHelper
             @mystyle = @mystyle + "-ms-filter: 'progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000)';"
         	@mystyle = @mystyle + "}"
 			if item.photo_id != nil
-		        @mystyle = @mystyle + "bgimg_<%= i %> { background-image: url('"+ url_for(Photo.find(item.photo_id).image) +"');"
+		        @mystyle = @mystyle + "bgimg { background-image: url('"+ url_for(Photo.find(item.photo_id).image) +"');"
 		        @mystyle = @mystyle + "background-repeat: no-repeat;"
         		@mystyle = @mystyle + "background-size: 100%; }"
   				@mystyle = @mystyle + ".bgcover { background: url("+ url_for(Photo.find(item.photo_id).image)+") no-repeat center;" 
@@ -38,7 +38,51 @@ module StylesHelper
     		end
       		@mystyle = @mystyle + "</style>"
       		@mystyle = @mystyle + "<link href=https://fonts.googleapis.com/css?family=" + "style.primary_font" +" rel='stylesheet'>"
+      		@mystyle = @mystyle + "<link href=https://fonts.googleapis.com/css?family=" + "style.secondary_font" +" rel='stylesheet'>"
+      		@mystyle = @mystyle + "<link href=https://fonts.googleapis.com/css?family=" + "style.alt_font" +" rel='stylesheet'>"
 
       		get_style = @mystyle
     end
+
+	def nget_style item, n
+		style = Style.find(item.style_id)
+    	@mystyle = "<style type='text/css'> .pf { font-family: " + style.primary_font + "; } "
+			@mystyle = @mystyle + ".sf_"+n.to_s+" { font-family: " + style.secondary_font + "; } "
+			@mystyle = @mystyle + ".af_"+n.to_s+" { font-family: " + style.alt_font + "; }"
+			@mystyle = @mystyle + ".pc_"+n.to_s+" { color: " + style.primary_color + "; }"
+			@mystyle = @mystyle + ".sc_"+n.to_s+" { color: " + style.secondary_color + "; }"
+			@mystyle = @mystyle + ".ac_"+n.to_s+" { color: " + style.alt_color + "; }"
+
+			@mystyle = @mystyle + ".bg_"+n.to_s+" { background-color: " + style.alt_color + "; }"
+
+			@mystyle = @mystyle + ".bgclear_"+n.to_s+" { background-color: transparent; }"
+
+			@mystyle = @mystyle + ".center_"+n.to_s+" {text-align: center; text-align: -webkit-center; }"
+
+			@mystyle = @mystyle + ".bglight_"+n.to_s+" {  background: rgb(0, 0, 0) transparent; background: rgba(0, 0, 0, 0.25);"
+			@mystyle = @mystyle + "filter:progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000);"
+            @mystyle = @mystyle + "-ms-filter: 'progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000)';"
+        	@mystyle = @mystyle + "}"
+			@mystyle = @mystyle + ".bgmed_"+n.to_s+" {  background: rgb(0, 0, 0) transparent; background: rgba(0, 0, 0, 0.5);"
+			@mystyle = @mystyle + "filter:progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000);"
+            @mystyle = @mystyle + "-ms-filter: 'progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000)';"
+        	@mystyle = @mystyle + "}"
+			@mystyle = @mystyle + ".bgdark_"+n.to_s+" {  background: rgb(0, 0, 0) transparent; background: rgba(0, 0, 0, 0.75);"
+			@mystyle = @mystyle + "filter:progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000);"
+            @mystyle = @mystyle + "-ms-filter: 'progid:DXImageTransform.Microsoft.gradient(startColorstr=#99000000, endColorstr=#99000000)';"
+        	@mystyle = @mystyle + "}"
+			if item.photo_id != nil
+		        @mystyle = @mystyle + "bgimg_"+n.to_s+" { background-image: url('"+ url_for(Photo.find(item.photo_id).image) +"');"
+		        @mystyle = @mystyle + "background-repeat: no-repeat;"
+        		@mystyle = @mystyle + "background-size: 100%; }"
+  				@mystyle = @mystyle + ".bgcover_"+n.to_s+" { background: url("+ url_for(Photo.find(item.photo_id).image)+") no-repeat center;" 
+   				@mystyle = @mystyle + "-webkit-background-size: cover;"
+  				@mystyle = @mystyle + "-moz-background-size: cover;"
+  				@mystyle = @mystyle + "-o-background-size: cover;"
+  				@mystyle = @mystyle + "background-size: 100% x 100%;}"
+    		end
+      		@mystyle = @mystyle + "</style>"
+      		get_style = @mystyle
+    end
+
 end
